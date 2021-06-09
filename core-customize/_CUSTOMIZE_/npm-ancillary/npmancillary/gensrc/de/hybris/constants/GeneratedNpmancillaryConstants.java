6f1a4f7a4a086bc0eb05622e4cec130b8339022c
Wang <wcathy2015@163.com>
2021-06-09 09:56:48 +0800

    0609 2

---
+++ core-customize/_CUSTOMIZE_/npm-ancillary/npmancillary/gensrc/de/hybris/constants/GeneratedNpmancillaryConstants.java
@@ -0,0 +1,26 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jun 8, 2021, 7:32:56 AM                     ---
+ * ----------------------------------------------------------------
+ *  
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package de.hybris.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedNpmancillaryConstants
+{
+	public static final String EXTENSIONNAME = "npmancillary";
+	
+	protected GeneratedNpmancillaryConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
